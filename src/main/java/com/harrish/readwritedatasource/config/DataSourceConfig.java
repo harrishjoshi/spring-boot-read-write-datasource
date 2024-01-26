9ca9f4b7078f1102ac0147090000e2d1969de646
harishjoshi <harish.joshi@8squarei.com>
2024-01-26 13:50:52 +0545

    feat: JPA vendor add

--- src/main/java/com/harrish/readwritedatasource/config/DataSourceConfig.java
+++ src/main/java/com/harrish/readwritedatasource/config/DataSourceConfig.java
@@ -9,17 +9,16 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -55,11 +54,16 @@ public class DataSourceConfig {
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(EntityManagerFactoryBuilder builder) {
-        return builder.dataSource(routingDataSource())
-                .packages("com.harrish.readwritedatasource.model")
-                .properties(jpaProperties())
-                .build();
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        var entityManager = new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(routingDataSource());
+        entityManager.setPackagesToScan("com.harrish.readwritedatasource.model");
+
+        var vendorAdapter = new HibernateJpaVendorAdapter();
+        entityManager.setJpaVendorAdapter(vendorAdapter);
+        entityManager.setJpaPropertyMap(jpaProperties());
+
+        return entityManager;
     }
 
     @Bean
